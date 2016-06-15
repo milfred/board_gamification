64d60b30f8e22c919d19d461a8d04af42b9bb24b
Grant Willey <email": null>
2016-06-15 17:48:08 -0500

    compelte

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'Faker'
+
+user_params = {username: Faker::Internet.user_name, email: Faker::Internet.email , password_digest: Faker::Internet.password , personal_info: Faker::Lorem.sentence}
+membership_params = {member_id: rand(10), group_id: rand(3) }
+group_params = {name: Faker::Space.star_cluster , creator_id: rand(10)}
+comment_params = {body: Faker::Hacker.say_something_smart , commenter_id: rand(10) , review_id: rand(20)}
+
+review_params = {rating: rand(5) , title: Faker::Commerce.product_name , body: Faker::Hacker.say_something_smart , reviewer_id: rand(10) , game_id: rand(10) }
+
+ownership_params = {owner_id: rand(10), game_id: rand(10) }
+
+game_params = {name: , description: Faker::Hacker.say_something_smart , number_of_players: rand(6), image_url: "https://metrouk2.files.wordpress.com/2015/06/kitten2.jpg" }
+
+categorization_params = {game_id: rand(7), category_id: rand(5)}
+
+category_params = {title: Faker::Book.genre }
+
+10.times {User.create(user_params)}
+
+35.times {Membership.create(membership_params)}
+
+3.times {Group.create(group_params)}
+
+30.times {Comment.create(comment_params)}
+
+40.times {Review.create(review_params)}
+
+25.times {Ownership.create(ownership_params)}
+
+7.times {Game.create(game_params)}
+
+10.times {Categorization.create(categorization_params)}
+
+5.times {Category.create(category_params)}
