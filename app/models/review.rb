86c787f917c740f18263f5852a3b68cbf9c6acba
Jason Milfred & Ryan Dempsey <pair+milfred.rmdemp@devbootcamp.com>
2016-06-15 16:34:52 -0500

    Add some validations, schema image

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :reviewer, class_name: "User"
   belongs_to :game
+
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
+  validates :title, presence: true
+  validates :reviewer_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   has_many :reviews, foreign_key: :reviewer_id
   has_many :ownerships, foreign_key: :owner_id
   has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
 end
