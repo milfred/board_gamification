210dc95f4afd57ecbe8d28ebc7a016837e00c35f
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-16 10:33:39 -0500

    Remove game and headers controller. PIVOT

--- app/controllers/headers_controller.rb
+++
@@ -1,5 +0,0 @@
-class HeadersController < ApplicationController
-  def index
-    @games = Game.search(params[:search])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  root "headers#index"
+  get '/games/results', to: 'games#results', as: "results"
+
+
   resources :sessions, only: [:create]
   get 'login' => 'sessions#new'
   delete 'logout' => 'sessions#destroy'
+
 
 
 
