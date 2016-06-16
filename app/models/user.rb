6661bd823814a5c452769f1f5207a0ed77f05188
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-16 14:25:45 -0500

    Update User associations to include owned_games

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :comments, foreign_key: :commenter_id
   has_many :reviews, foreign_key: :reviewer_id
   has_many :ownerships, foreign_key: :owner_id
+  has_many :owned_games, through: :ownerships, source: :game
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
