3f63b453969a031ebefc780483ab896fead75142
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-16 17:51:01 -0500

    Insert if/else on header for different views if the user is logged in or not.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   protect_from_forgery with: :exception
 
+
+
 private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def logged_in?
-     session[:user_id] != nil
-   end
 end
