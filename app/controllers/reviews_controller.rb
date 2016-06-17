3c5ae24f0b3633d566a84aee3037974e757e5aa6
Grant Willey <email": null>
2016-06-16 19:56:17 -0500

    Create, read, delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,17 @@
 class ReviewsController < ApplicationController
   def create
-    @review = Review.new
+    @review = Review.new(title: params[:review][:title], body: params[:review][:body], rating: params[:review][:rating])
     @review.game_id = params[:game_id]
     @review.reviewer_id = current_user.id
     if @review.save
+      p @review
       redirect_to "/games/#{params[:game_id]}"
     else
-      "bananas"
+
     end
   end
 
   def new
-
   end
 
   def edit
@@ -21,6 +21,9 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to "/games/#{review.game_id}"
   end
 
 end
