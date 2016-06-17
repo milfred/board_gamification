abeb74a5960443bc710c7f38ee3ad9896ac54ae0
Grant Willey <email": null>
2016-06-17 10:02:26 -0500

    Average rating for game

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def average_rating
+    sum = @reviews.map { |review| review.rating }.reduce(:+)
+    average = sum / @reviews.length.to_f
+    average.round(2)
+  end
 end
